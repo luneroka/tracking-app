73f8baabe5d668b07b9dec123dcf30739784224e
Luneroka <luneroka@gmail.com>
2025-09-25 14:16:32 +0200

    Add base layer styles

--- app/components/TicketCard.tsx
+++ app/components/TicketCard.tsx
@@ -5,9 +5,19 @@ import StatusDisplay from './StatusDisplay';
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
-      <PriorityDisplay />
+    <div className='flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2'>
+      <div className='flex mb-3 '>
+        <PriorityDisplay />
+        <div className='ml-auto'>
+          <DeleteBlock />
+        </div>
+      </div>
+
+      <h4>Ticket Title</h4>
+      <hr className='h-px border-0 bg-page mb-2' />
+      <p className='whitespace-pre-wrap'>
+        This is the ticket description! Please take care of it.
+      </p>
       <ProgressBar />
       <StatusDisplay />
     </div>
