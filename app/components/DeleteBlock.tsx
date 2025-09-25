bd373b26407569b3d6eb319a8a3a8c89b3c0dca4
Luneroka <luneroka@gmail.com>
2025-09-25 17:16:52 +0200

    Wire DELETE route to DeleteBlock component

--- app/components/DeleteBlock.tsx
+++ app/components/DeleteBlock.tsx
@@ -1,7 +1,31 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import { FaXmark } from 'react-icons/fa6';
 
-const DeleteBlock = () => {
-  return <FaXmark className='text-red-400 hover:text-red-200 cursor-pointer' />;
+interface DeleteBockProps {
+  id: string;
+}
+
+const DeleteBlock = ({ id }: DeleteBockProps) => {
+  const router = useRouter();
+
+  const deleteTicket = async () => {
+    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (res.ok) {
+      router.refresh();
+    }
+  };
+
+  return (
+    <FaXmark
+      className='text-red-400 hover:text-red-200 cursor-pointer'
+      onClick={deleteTicket}
+    />
+  );
 };
 
 export default DeleteBlock;
